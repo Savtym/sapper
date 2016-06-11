9036933c03fb49bc097ced0249c17ae93349fe6f
Savtym <savtym@gmail.com>
2016-06-11 20:30:58 +0300

    preparation

---
+++ script/script.js
@@ -0,0 +1,25 @@
+var x 		= 500;
+var y 		= 500;
+var array 	= [];
+
+
+var game 	= document.getElementById('game');
+var canvas 	= document.createElement('canvas');
+
+
+
+
+canvas.setAttribute('width', x);
+canvas.setAttribute('height', y);
+canvas.setAttribute('id', 'canvas');
+game.appendChild(canvas);
+
+context = canvas.getContext("2d");
+
+
+function preparation() {
+	var side = 18;
+	
+
+}
+
